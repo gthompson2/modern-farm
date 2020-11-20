8008678f27699972154bfbfb4d9c6e7e194d7713
Gregory Thompson <mrgregorythompsonii@gmail.com>
2020-11-20 10:42:01 -0600

    added seed procucing modules

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,19 @@
-console.log("Welcome to the main module")
+console.log("Welcome to the main module");
 
+import { createPlan } from './plan.js';
+
+
+const yearlyPlan = createPlan();
+
+console.log(yearlyPlan);
+
+// test code:
+import { createAsparagus } from './seeds/asparagus.js';
+import { createCorn } from './seeds/corn.js';
+
+const asparagusSeed = createAsparagus();
+const cornSeed = createCorn();
+
+console.log (asparagusSeed);
+console.log(cornSeed);
+// end test code:
---
+++ src/scripts/seeds/sunflower.js
@@ -0,0 +1,8 @@
+export const createSunflower = () => {
+    return {
+        type: "Sunflower",
+        height: "180",
+        output: "3"
+    }
+       
+}
